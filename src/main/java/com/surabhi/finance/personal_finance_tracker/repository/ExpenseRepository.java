e3264d3c2f8107fa46b095f82d445ff574b00921
surabhi-acharjee <surabhi.acharjee20@gmail.com>
2024-06-30 21:36:44 -0500

    2nd version

--- src/main/java/com/surabhi/finance/personal_finance_tracker/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.surabhi.finance.personal_finance_tracker.repository;
-
-import com.surabhi.finance.personal_finance_tracker.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-}
